260bec5d149f2a1502e49dddfb800d32b30362f5
pabloyokese <jpo_54@hotmail.com>
2018-07-18 23:18:24 +0800

    adding data connection
    adding data connection in properties file

---
+++ src/main/java/com/jp/batchexample/BatchexampleApplication.java
@@ -0,0 +1,12 @@
+package com.jp.batchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchexampleApplication.class, args);
+    }
+}
